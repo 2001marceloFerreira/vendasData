2689d8eaaf7eceaf49c5f50de60b23b9afde49d9
marcelo.ferreira.canal <marcelo.ferreira.canal@gmail.com>
2022-09-08 16:32:14 -0300

    project created

---
+++ backend/src/main/java/com/vendas/vendasdata/VendasdataApplication.java
@@ -0,0 +1,13 @@
+package com.vendas.vendasdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendasdataApplication.class, args);
+	}
+
+}
