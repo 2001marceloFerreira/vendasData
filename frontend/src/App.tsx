f94e09d6e8ce5815cd44b1f703e097feaa32e8cc
marcelo.ferreira.canal <marcelo.ferreira.canal@gmail.com>
2022-09-08 21:40:15 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,20 @@
+import Header from "./components/Header"
 import NotificationButton from "./components/NotificationButton"
+import SalesCard from "./components/SalesCard"
 
 function App() {
   return (
     <>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
       <h1>hellow wordd</h1>
-      <NotificationButton />
-      <NotificationButton />
+
     </>
   )
 }
