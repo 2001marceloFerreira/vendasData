ca3887a18dc94f27e8838b1bc4cd887aa938a736
marcelo.ferreira.canal <marcelo.ferreira.canal@gmail.com>
2022-09-10 14:16:08 -0300

    send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,9 +1,25 @@
 import icon from '../../assets/img/notificacao-icon.svg';
+import axios from 'axios';
+import { BASE_URL } from '../../utils/request';
+
 import './styles.css';
 
-function NotificationButton() {
+type Props = {
+    saleId: number;
+}
+
+function handleClick(id: number) {
+    axios(`${BASE_URL}/sales/notification/${id}`)
+        .then(response => {
+            alert('SUCESSO');
+        });
+}
+
+
+function NotificationButton({ saleId }: Props) {
+
     return (
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
             <img src={icon} alt="Notificar" />
         </div>
     )
